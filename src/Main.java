35d0e2459ed10c6310cf07e7af53793e133f76e7
Piyush Srivastava <piyush.srivastava@office365.3pillarglobal.com>
2022-11-05 13:18:00 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(5.0,4.0));
+    }
+    public static double area(double radius) {
+        if(radius<0) {
+            return -1.0;
+        }
+        return (Math.PI*radius*radius);
+    }
+    public static double area(double x,double y) {
+        if((x<0) || (y<0)) {
+            return -1.0;
+        }
+        return (x*y);
+    }
+}
